e003e9dbdcc748eb23c80393ef3628d6ff2c7802
Alex Winterlee <alexwinterlee@gmail.com>
2023-05-26 16:00:59 +0000

    Signup, Login, Create Account - Complete

--- sample_service/routers/accounts.py
+++ sample_service/routers/accounts.py
@@ -1,1 +1,84 @@
-#hi
+from fastapi import (
+    Depends,
+    HTTPException,
+    status,
+    Response,
+    APIRouter,
+    Request,
+)
+from jwtdown_fastapi.authentication import Token
+from authentication import authenticator
+
+from pydantic import BaseModel
+
+from queries.accounts import (
+    AccountIn,
+    AccountOut,
+    AccountQueries,
+    DuplicateAccountError,
+    AccountOutWithPassword,
+)
+
+
+class AccountForm(BaseModel):
+    username: str
+    password: str
+
+
+class AccountToken(Token):
+    account: AccountOut
+
+
+class HttpError(BaseModel):
+    detail: str
+
+
+router = APIRouter()
+
+
+@router.get("/token", response_model=AccountToken | None)
+async def get_token(
+    request: Request,
+    account: AccountIn = Depends(authenticator.try_get_current_account_data)
+) -> AccountToken | None:
+    if account and authenticator.cookie_name in request.cookies:
+        return {
+            "access_token": request.cookies[authenticator.cookie_name],
+            "type": "Bearer",
+            "account": account,
+        }
+
+
+@router.get("/api/accounts/{username}", response_model=AccountOut | HttpError)
+async def get_account(
+    username: str,
+    response: Response,
+    queries: AccountQueries = Depends(),
+):
+    record = queries.get_account(username)
+    if record is None:
+        response.status_code = 404
+    else:
+        return record
+
+
+@router.post("/api/accounts", response_model=AccountToken | HttpError)
+async def create_account(
+    info: AccountIn,
+    request: Request,
+    response: Response,
+    repo: AccountQueries = Depends(),
+):
+    hashed_password = authenticator.hash_password(info.password)
+    try:
+        account = repo.create_account(info, hashed_password)
+        form = AccountForm(username=info.username, password=info.password)
+        token = await authenticator.login(response, request, form, repo)
+        return AccountToken(account=account, **token.dict())
+        # return account
+    #todo: add auto login feature
+    except DuplicateAccountError:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Cannot create an account with those credentials",
+        )
