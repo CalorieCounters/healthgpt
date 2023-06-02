3fc02542f3c2e05396466ad331adde3c23d8908b
awinterlee <alexwinterlee@gmail.com>
2023-06-02 19:26:58 -0400

    fully working log a meal, slightly more pretty but not yet there

--- ghi/src/MealForm.js
+++ ghi/src/MealForm.js
@@ -9,6 +9,9 @@ const MealForm = () => {
   const [name, setName] = useState("");
   const [type, setType] = useState("");
   const [dateTimeEaten, setDateTimeEaten] = useState("");
+  const [showNutrients, setShowNutrients] = useState(false);
+  const [saveButton, setSaveButton] = useState(false);
+  const [refreshButton, setRefreshButton] = useState(false);
 
   const foodGroups = [
     "Zero",
@@ -48,6 +51,16 @@ const MealForm = () => {
     setType(value);
   };
 
+  const handleSaveButtonChange = (event) => {
+    setSaveButton(true);
+    console.log("test", saveButton);
+  };
+
+  const handleRefreshButtonChange = (event) => {
+    setSaveButton(false);
+    setShowNutrients(false);
+  };
+
   const fetchData = async () => {
     const nutrientUrl = `http://localhost:8000/natural/nutrients`;
 
@@ -69,6 +82,7 @@ const MealForm = () => {
 
     setFoodItems(response);
     setQuery("");
+    setShowNutrients(true);
   };
 
   const handleSeeNutrients = async (event) => {
@@ -78,6 +92,8 @@ const MealForm = () => {
   };
 
   const handleLog = async (event) => {
+    setSaveButton(false);
+    setShowNutrients(false);
     event.preventDefault();
     // CREATING A MEAL, BUT WE STILL NEED TO ADD FOOD ITEMS TO THE MEAL
     // TODO: Finish creating meal form with these fields
@@ -151,115 +167,161 @@ const MealForm = () => {
       <div className="offset-3 col-6">
         <h1 className="text-center">Log a Meal</h1>
         <form onSubmit={handleSeeNutrients} id="create-appointment-form">
-          <div className="form-floating mb-3">
-            <input
-              value={name}
-              onChange={handleNameChange}
-              placeholder="name"
-              name="name"
-              required
-              type="text"
-              id="name"
-              className="form-control"
-            />
-            <label htmlFor="ends">Name</label>
-          </div>
-          <div className="form-floating mb-3">
-            <input
-              value={description}
-              onChange={handleDescriptionChange}
-              placeholder="description"
-              name="description"
-              required
-              type="text"
-              id="description"
-              className="form-control"
-            />
-            <label htmlFor="ends">Description</label>
-          </div>
-          <div className="form-floating mb-3">
-            <input
-              value={type}
-              onChange={handleTypeChange}
-              placeholder="name"
-              name="name"
-              required
-              type="text"
-              id="name"
-              className="form-control"
-            />
-            <label htmlFor="ends">
-              Type: (Breakfast, Lunch, Dinner, or Snack)
-            </label>
-          </div>
-          <div className="form-floating mb-3">
-            <input
-              value={dateTimeEaten}
-              onChange={handleDateTimeEatenChange}
-              placeholder="dateTimeEaten"
-              name="dateTimeEaten"
-              required
-              type="date"
-              id="datetime_eaten"
-              className="form-control"
-            />
-            <label htmlFor="ends">Date Eaten</label>
-          </div>
-          <div className="form-floating mb-3">
-            <textarea
-              onChange={handleQueryChange}
-              placeholder="Description"
-              required
-              type="text"
-              name="Description"
-              id="Description"
-              className="form-control"
-              value={query}
-            />
-            <label htmlFor="first_name">What was in your meal...</label>
-          </div>
-          <button className="btn btn-primary w-100">See Nutrients</button>
+          {saveButton && (
+            <div className="form-floating mb-3">
+              <input
+                value={name}
+                onChange={handleNameChange}
+                placeholder="name"
+                name="name"
+                required
+                type="text"
+                id="name"
+                className="form-control"
+              />
+              <label htmlFor="ends">Name</label>
+            </div>
+          )}
+          {saveButton && (
+            <div className="form-floating mb-3">
+              <input
+                value={description}
+                onChange={handleDescriptionChange}
+                placeholder="description"
+                name="description"
+                required
+                type="text"
+                id="description"
+                className="form-control"
+              />
+              <label htmlFor="ends">Description</label>
+            </div>
+          )}
+          {saveButton && (
+            <div className="form-floating mb-3">
+              <select
+                value={type}
+                onChange={handleTypeChange}
+                placeholder="name"
+                name="name"
+                required
+                type="text"
+                id="name"
+                className="form-control"
+              >
+                <option value="">Select a type</option>
+                <option value="Breakfast">Breakfast</option>
+                <option value="Lunch">Lunch</option>
+                <option value="Dinner">Dinner</option>
+                <option value="Snack">Snack</option>
+                <option value="Dessert">Dessert</option>
+              </select>
+              <label htmlFor="ends">Type of Meal</label>
+            </div>
+          )}
+          {saveButton && (
+            <div className="form-floating mb-3">
+              <input
+                value={dateTimeEaten}
+                onChange={handleDateTimeEatenChange}
+                placeholder="dateTimeEaten"
+                name="dateTimeEaten"
+                required
+                type="date"
+                id="datetime_eaten"
+                className="form-control"
+              />
+              <label htmlFor="ends">Date Eaten</label>
+            </div>
+          )}
+          {!showNutrients && (
+            <div className="form-floating mb-3">
+              <textarea
+                onChange={handleQueryChange}
+                placeholder="Description"
+                required
+                type="text"
+                name="Description"
+                id="Description"
+                className="form-control"
+                value={query}
+              />
+              <label htmlFor="first_name">What was in your meal...</label>
+            </div>
+          )}
+          {!showNutrients && (
+            <button className="btn btn-primary w-100">See Nutrients</button>
+          )}
         </form>
-
-        <table className="table table-striped">
-          <thead>
-            <tr>
-              <th>Image</th>
-              <th>Amount</th>
-              <th>Food item</th>
-              <th>Calories</th>
-              <th>Food Group</th>
-            </tr>
-          </thead>
-          <tbody>
-            {foodItems.map((foodItem, index) => {
-              return (
-                <tr key={`${foodItem.food_name}_${index}`} value={foodItem.id}>
-                  <td>
-                    <img
-                      style={{ width: "100px" }}
-                      alt="food item"
-                      src={
-                        foodItem.photo.thumb ||
-                        "https://datalabel.com/wp-content/uploads/2022/03/placeholder-image-300x225-1.png"
-                      }
-                    />
-                  </td>
-                  <td>
-                    {foodItem.serving_qty} {foodItem.serving_unit}
-                  </td>
-                  <td>{foodItem.food_name}</td>
-                  <td>{foodItem.nf_calories}</td>
-                  <td>{foodGroups[foodItem.tags.food_group]}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-
-        <button onClick={handleLog} className="btn btn-success w-100">
-          Log Meal
-        </button>
+        {showNutrients && (
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <th>Image</th>
+                <th>Amount</th>
+                <th>Food item</th>
+                <th>Calories</th>
+                <th>Food Group</th>
+              </tr>
+            </thead>
+            <tbody>
+              {foodItems.map((foodItem, index) => {
+                return (
+                  <tr
+                    key={`${foodItem.food_name}_${index}`}
+                    value={foodItem.id}
+                  >
+                    <td>
+                      <img
+                        style={{ width: "100px" }}
+                        alt="food item"
+                        src={
+                          foodItem.photo.thumb ||
+                          "https://datalabel.com/wp-content/uploads/2022/03/placeholder-image-300x225-1.png"
+                        }
+                      />
+                    </td>
+                    <td>
+                      {foodItem.serving_qty} {foodItem.serving_unit}
+                    </td>
+                    <td>{foodItem.food_name}</td>
+                    <td>{foodItem.nf_calories}</td>
+                    <td>{foodGroups[foodItem.tags.food_group]}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        )}
+        {saveButton && (
+          <button onClick={handleLog} className="btn btn-success w-100">
+            Log Meal
+          </button>
+        )}
+        {showNutrients && !saveButton && (
+          <button
+            onClick={handleSaveButtonChange}
+            className="btn btn-primary w-100"
+          >
+            Save Meal
+          </button>
+        )}
+        {showNutrients && !saveButton && (
+          <button
+            onClick={handleRefreshButtonChange}
+            className="btn btn-primary w-100"
+          >
+            Return to lookup
+          </button>
+        )}
+        {saveButton && (
+          <button
+            onClick={handleRefreshButtonChange}
+            className="btn btn-primary w-100"
+          >
+            Return to lookup
+          </button>
+        )}
       </div>
     </div>
   );
