a5c8f8c5ff51df42a46d6d9e24a99fe22f4dde9a
Italiz Vazquez <italizv@gmail.com>
2023-06-07 15:58:14 +0000

    Exercise Tests

--- sample_service/queries/test_exercises.py
+++ sample_service/queries/test_exercises.py
@@ -1,20 +1,17 @@
+from queries.exercises import ExerciseQueries
 from .utils import map_fields_to_array
 from fastapi.testclient import TestClient
 from authentication import authenticator
 from main import app
-from models.exercises import Exercise, ExerciseCreateOut
-from queries.exercises import ExerciseQueries
-from fastapi.encoders import jsonable_encoder
 
 client = TestClient(app)
 
 
-def test_exercise_map_fields():
-    mapped = map_fields_to_array([], [])
-
-    expected_result = []
-
-    assert mapped == expected_result
+class CreateExeriseQueries:
+    def create(self, exercises, user_id):
+        result = {"id": 1, "user_id": user_id}
+        print(result)
+        return {"message": "Success", "count": 2}
 
 
 def fake_get_current_account_data(account: dict):
@@ -22,39 +19,91 @@ def fake_get_current_account_data(account: dict):
     return account
 
 
-class FakeCreateExercise:
-    def create(self, exercises, user_id):
-        pass
-
-
 def test_create_exercise():
+    app.dependency_overrides[ExerciseQueries] = CreateExeriseQueries
     app.dependency_overrides[
         authenticator.get_current_account_data
     ] = fake_get_current_account_data
-    app.dependency_overrides[ExerciseQueries] = FakeCreateExercise
 
-    input = [
-        Exercise(name_type="running", duration=30.2, burned_calories=302.25),
-        Exercise(name_type="running", duration=30.2, burned_calories=302.25),
-    ]
-    json_compatible_item_data = jsonable_encoder(input)
-    print("JSON ENCODED", json_compatible_item_data)
+    input = {
+        "exercises": [
+            {
+                "name_type": "biking",
+                "duration": 30.2,
+                "burned_calories": 302.25,
+            },
+            {
+                "name_type": "swimming",
+                "duration": 30.2,
+                "burned_calories": 302.25,
+            },
+        ]
+    }
+
     response = client.post(
         "/exercise",
-        headers={
-            "Authorization": "Bearer 123",
-        },
-        json=json_compatible_item_data,
+        json=input,
     )
-    print("RESPNSE", response.text)
+    app.dependency_overrides = {}
     assert response.status_code == 200
-    assert response.json() == ExerciseCreateOut(message="Success", count=1)
+    assert response.json() == {"message": "Success", "count": 2}
 
 
-def test_main_exercises():
-    response = client.get("/")
+def test_create_exercise_missing_field():
+    app.dependency_overrides[
+        authenticator.get_current_account_data
+    ] = fake_get_current_account_data
 
-    expected = {"message": "Hello World"}
+    input = {
+        "exercises": [
+            {
+                # missing name_type
+                "duration": 30.2,
+                "burned_calories": 302.25,
+            }
+        ]
+    }
+
+    response = client.post(
+        "/exercise",
+        json=input,
+    )
+    app.dependency_overrides = {}
+    assert response.status_code == 422
+    assert response.json() == {
+        "detail": [
+            {
+                "loc": ["body", "exercises", 0, "name_type"],
+                "msg": "field required",
+                "type": "value_error.missing",
+            }
+        ]
+    }
+
+
+def test_create_exercise_empty_list():
+    app.dependency_overrides[
+        authenticator.get_current_account_data
+    ] = fake_get_current_account_data
+
+    input = {"exercises": []}
+
+    response = client.post(
+        "/exercise",
+        json=input,
+    )
 
     assert response.status_code == 200
-    assert response.json() == expected
+    assert response.json() == {"message": "Success", "count": 0}
+
+
+def test_exercise_map_fields():
+    mapped = map_fields_to_array(
+        [["running", 30, 300]], ["name_type", "duration", "burned_calories"]
+    )
+
+    expected_result = [
+        {"name_type": "running", "duration": 30, "burned_calories": 300}
+    ]
+
+    assert mapped == expected_result
